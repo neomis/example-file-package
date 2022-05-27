951eb110f0e2232a1ca085eb2079d3c227e4a2ff
Justin Blair <neomis@gmail.com>
2022-05-27 08:56:05 -0700

    Initial commit

---
+++ python_template/__version__.py
@@ -0,0 +1,9 @@
+"""Package meta data."""
+__title__ = 'python-template'
+__description__ = 'Python Package Template.'
+__url__ = 'https://github.com/neomis/python-template'
+__version__ = '1.1.2'
+__author__ = 'neomis'
+__author_email__ = 'neomis@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022 neomis'
